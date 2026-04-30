8b9dad5752b1283c89264095f8428b6a3c62f180
mirkoferretti1 <mirko.ferretti@outlook.com>
2026-04-30 15:01:34 +0200

    Create numero.R

---
+++ R/numero.R
@@ -0,0 +1,12 @@
+#if else
+numero <-function(x) {
+  if(x>0) {
+    "Questo numero è positivo"
+  }
+  else if(x<0) {
+    "Questo numero è negativo"
+  } 
+  else {
+    "Zero non è né positivo né negativo"
+  }
+}
